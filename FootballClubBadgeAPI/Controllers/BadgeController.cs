ae6e570cdf94202161efae415b18fdc130fa2d58
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-15 15:35:23 +0000

    setup different connection strings depending on the environment and also added repository logic for adding a match to the database

---
+++ FootballClubBadgeAPI/Controllers/BadgeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballClubBadgeAPI.Controllers
+{
+    [Route("api/badge")]
+    [ApiController]
+    public class BadgeController : ControllerBase
+    {
+    }
+}
