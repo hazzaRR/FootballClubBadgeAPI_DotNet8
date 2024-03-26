679d643eee3b872e336e7f929212e1cd9b428eec
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-26 17:50:50 +0000

    added test that moqs IImageStorageService and tests GetTeams method returns an OkObjectResult and the returned value matches the passed value

--- FootballClubBadgeAPI.Tests/BadgeControllerTests.cs
+++ FootballClubBadgeAPI.Tests/BadgeControllerTests.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using FootballClubBadgeAPI.Controllers;
+using FootballClubBadgeAPI.Interfaces;
+using System;
+using Moq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FootballClubBadgeAPI.Tests
 {
     public class BadgeControllerTests
     {
+
+        private readonly BadgeController _badgeController;
+        private readonly Mock<IImageStorageService> _imageStorageService;
+
+
+        public BadgeControllerTests()
+        {
+
+            _imageStorageService = new Mock<IImageStorageService>();
+            _badgeController = new BadgeController(_imageStorageService.Object);
+            
+        }
 
         [Fact]
         public void GetTeams_ReturnsList()
@@ -15,11 +31,21 @@ namespace FootballClubBadgeAPI.Tests
 
             //Arrange
 
+            _imageStorageService.Setup(service => service.GetTeamBadgeFilenamesAsync())
+                .Returns(new string[] { "Derby County", "Sheffield Wednesday" });
 
             //Act
 
+            var result = _badgeController.GetTeams();
 
             //Assert
+
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+
+            Assert.NotNull(okResult.Value);
+            Assert.Equal(new string[] { "Derby County", "Sheffield Wednesday" }, okResult.Value);
+
 
         } 
     }
